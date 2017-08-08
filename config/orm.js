7026371350126b6672973ee5436c30c87274a26d
Benjamin Owens <Benjamin.Owens@amtrustgroup.com>
2017-08-07 20:53:21 -0400

    working

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,62 @@
-var connection = require ("./connection.js")
+var connection = require ("./connections.js")
+
+
+
+
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
+// Object for all our SQL statement functions.
+var orm = {
+  show: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+//cb is a function that got passed as an argument
+  add: function(nameofit, cb) { 
+     var mySQLQuery = "INSERT INTO burgers (burger_name) VALUES ('" + nameofit + "')"; 
+     connection.query(mySQLQuery, function(err, result) { 
+       if (err) throw err; 
+       cb(result); 
+     }); 
+   }, 
+
+///www is a generic name of a function that comes from 
+   update: function(baz, www){
+	var mySQLQuery = "UPDATE burgers SET devoured = 1 WHERE id = "+baz+""; 
+
+	connection.query(mySQLQuery, function(err, result) { 
+       if (err) throw err; 
+       www(result); 
+     }); 
+   }
+};
+
+// Export the orm object for the model
+module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -2,38 +2,28 @@
 //this is the  my orm should connect to the orm.  the query stuff will come from the orm.
 //var orm = require("../config/config.js")
 
-let connection = require('../config/connections.js');
+var orm = require('../config/orm.js');
 
 let burger = {
-	show : function(callback){
-		connection.query("SELECT * FROM burgers;", function(err, data) {
-		    if (err) {
-		      throw err;
-		    }
-		    return callback(data);
-		});
-	},
-	add : function(name, callback){
-		console.log (name)
-	  connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [name], function(err, result) {
-	    if (err) {
-	      throw err;
-	    }
-	    callback(result);
-	  });		
-	},
 
-	update : function(id, callback){
 
-	  connection.query("UPDATE burgers SET devoured = 1 WHERE id = ?", [id], function(err, result) {
-	  	console.log("made it to models file")
-	    if (err) {
-	      throw err;
-	    }
 
-	    callback(result);
-	  });
-	}
-};
+	show: function(cb) { 
+ 		orm.show('burgers', function(res){ 
+ 			cb(res); 
+ 		}); 
+	}, 
+
+	add: function (name, kallbak) { 
+     orm.add(name, function (res){ 
+       kallbak(res); 
+     }); 
+  }, 
+	update: function (name, kallbak) { 
+     orm.update(name, function (res){ 
+       kallbak(res); 
+     }); 
+  }
+}
 
 module.exports = burger;
