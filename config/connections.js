917235ec09e5e75823ee81aecb4bcf2a8f03249a
Benjamin Owens <Benjamin.Owens@amtrustgroup.com>
2017-08-17 19:22:31 -0400

    after modifyhing connections

--- config/connections.js
+++ config/connections.js
@@ -1,11 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var database_config;
+if (process.env.DATABASE_URL) 
+{
+	database_config = process.env.DATABASE_URL;
+} else 
+{
+	database_config = {
+	  host: "localhost",
+	  user: "root",
+	  password: "",
+	  database: "burgers_db"
+	}
+}
+var connection = mysql.createConnection(database_config);
 
 connection.connect(function(err) {
   if (err) {
